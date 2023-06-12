079e9c218ca30413e39bb6b37396c1aeb37cfbaa
lavumi <lavumi@gmail.com>
2023-06-12 15:06:40 +0900

    re-rendering by tile chunk size

--- src/resources/tile_map_storage.rs
+++ src/resources/tile_map_storage.rs
@@ -1,53 +1,102 @@
 use crate::components::tile::{InstanceTileRaw, Tile};
 
-pub struct TileMapStorage {
-    tiles: Vec<Tile>,
-    meshes: Vec<InstanceTileRaw>,
+
+struct TileChunk {
+    pub center_position: [f32; 2],
+    pub meshes: Vec<InstanceTileRaw>,
 }
 
 
-impl Default for TileMapStorage{
+impl TileChunk {
+    pub fn new(center_position: [f32; 2], chunk_size: i32) -> Self {
+        let meshes = (-chunk_size..chunk_size).flat_map(|x| {
+            (-chunk_size..chunk_size).map(move |y| {
+                let tile = ((center_position[0] + center_position[1]) / 20.0) as u8 % 4;
+                (Tile {
+                    tile_index: [0, tile as u8],
+                    uv_size: [0.02857, 0.024390],
+                    position: [x  as f32 + center_position[0], y  as f32 + center_position[1], 0.0],
+                    texture: "world".to_string(),
+                    flip: false,
+                }).to_tile_raw()
+            })
+        }).collect::<Vec<_>>();
+
+        TileChunk {
+            center_position,
+            meshes,
+        }
+    }
+}
+
+
+pub struct TileMapStorage {
+    tiles: Vec<TileChunk>,
+    meshes: Vec<InstanceTileRaw>,
+    chunk_size: i32,
+    camera_pos : [f32;2]
+}
+
+
+impl Default for TileMapStorage {
     fn default() -> Self {
-        let tiles =
-            (-100..100).flat_map(|x| {
-                (-100..100).map(move |y| {
-                    let tile = (y / 10) % 4;
-                    Tile{
-                        tile_index: [0,tile as u8 ],
-                        uv_size: [0.02857, 0.024390],
-                        position: [(x*2) as f32,(y*2) as f32,0.0],
-                        texture: "world".to_string(),
-                        flip: false,
-                    }
-                })
-            }).collect::<Vec<_>>();
+        let full_map_size = 20;
+        let chunk_size = 5;
+        let tiles = (-full_map_size..full_map_size).flat_map(|x| {
+            (-full_map_size..full_map_size).map(move |y| {
+                TileChunk::new([(x  * chunk_size )as f32 * 2., (y  * chunk_size ) as f32 * 2.0], chunk_size)
+            })
+        }).collect::<Vec<_>>();
 
 
-        TileMapStorage{
+        let camera_pos = [0.0,0.0];
+        let mut meshes = vec![];
+        for tile in tiles.iter() {
+            if tile.center_position[0] < camera_pos[0] + chunk_size as f32 * 3.0 &&
+                tile.center_position[0] > camera_pos[0] - chunk_size as f32 * 3.0 &&
+                tile.center_position[1] < camera_pos[1] + chunk_size as f32 * 3.0 &&
+                tile.center_position[1] > camera_pos[1] - chunk_size as f32 * 3.0
+            {
+               meshes.extend(tile.meshes.iter());
+            }
+        }
+
+
+
+        TileMapStorage {
             tiles,
-            meshes: vec![],
+            meshes,
+            chunk_size,
+            camera_pos
         }
     }
 }
 
 
 impl TileMapStorage {
-    pub fn get_meshes(& self) -> Vec<InstanceTileRaw>{
+    pub fn get_meshes(&self) -> Vec<InstanceTileRaw> {
         self.meshes.clone()
     }
 
-    pub fn update_tile_grid(&mut self, camera_pos: [f32;2]){
-        self.meshes.clear();
-        for tile in self.tiles.iter() {
-            if
-            tile.position[0] < camera_pos[0] + 10.0 &&
-                tile.position[0] > camera_pos[0] - 10.0 &&
-                tile.position[1] < camera_pos[1] + 10.0 &&
-                tile.position[1] > camera_pos[1] - 10.0
-            {
-                self.meshes.push(tile.to_tile_raw());
-            }
+    pub fn update_tile_grid(&mut self, camera_pos: [f32; 2]) {
+        if self.camera_pos[0] < camera_pos[0] + self.chunk_size as f32  &&
+            self.camera_pos[0] > camera_pos[0] - self.chunk_size as f32  &&
+            self.camera_pos[1] < camera_pos[1] + self.chunk_size as f32  &&
+            self.camera_pos[1] > camera_pos[1] - self.chunk_size as f32
+        {
+            return;
         }
 
+        self.camera_pos = camera_pos;
+        self.meshes.clear();
+        for tile in self.tiles.iter() {
+            if tile.center_position[0] < camera_pos[0] + self.chunk_size as f32 * 3.0 &&
+                tile.center_position[0] > camera_pos[0] - self.chunk_size as f32 * 3.0 &&
+                tile.center_position[1] < camera_pos[1] + self.chunk_size as f32 * 3.0 &&
+                tile.center_position[1] > camera_pos[1] - self.chunk_size as f32 * 3.0
+            {
+                self.meshes.extend(tile.meshes.iter());
+            }
+        }
     }
 }
--- src/system/update_player.rs
+++ src/system/update_player.rs
@@ -38,7 +38,7 @@ impl<'a> System<'a> for UpdatePlayer {
                 movement[0] += dt.0 * speed;
             }
             tile.move_tile(movement);
-            //todo 이건 플레이어가 하나뿐일때만 동작하는 멍청한 코드인데...
+            //todo 이건 플레이어가 하나뿐 일때만 작동하는 멍청한 코드인데...
             //좀 스마트한 방법 없을까요
             let camera_pos = camera.move_camera(movement);
             tile_map_storage.update_tile_grid(camera_pos);
