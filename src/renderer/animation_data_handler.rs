51755e61a446ae2c26ecace5d0580e7e08013d00
lavumi <lavumi@gmail.com>
2023-07-27 12:02:05 +0900

    character animation ongoing output new animation atlas

--- src/renderer/animation_data_handler.rs
+++ src/renderer/animation_data_handler.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 use log::info;
 use serde::Deserialize;
 use wgpu::util::DeviceExt;
+
 use crate::renderer::{Texture, Vertex};
 
 #[derive(Debug, Deserialize)]
@@ -99,16 +100,20 @@ impl AnimationDataHandler {
 
 
     pub async fn render_test(&self) -> Result<(), wgpu::SurfaceError> {
+
+
+        //todo 그냥 renderState 에서 가져와도 된다
+        // region [ Init Render Device ]
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             dx12_shader_compiler: Default::default(),
         });
         let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: None,
-                force_fallback_adapter: false,
-            })
+                .request_adapter(&wgpu::RequestAdapterOptions {
+                    power_preference: wgpu::PowerPreference::default(),
+                    compatible_surface: None,
+                    force_fallback_adapter: false,
+                })
             .await
             .unwrap();
         let (device, queue) = adapter
@@ -116,69 +121,15 @@ impl AnimationDataHandler {
             .await
             .unwrap();
 
+        //endregion
 
-        let str = fs::read_to_string("./assets/character/06_1.json").expect("Unable to read file");
-        let data: AnimationJsonData = serde_json::from_str(&str).expect("JSON was not well-formatted");
 
-        let sprite_length = data.frames.len();
         let texture_size = [
-            64u32 * sprite_length as u32, 64u32
+            64u32 * 53 as u32, 64u32 * 8
         ];
 
 
-        let mut vertex: Vec<Vertex> = vec![];
-        let mut indices: Vec<u16> = vec![];
-
-        for vertex_index in 0..sprite_length as u16 {
-            let frame_data = &data.frames[vertex_index as usize];
-            let original_uv = [
-                frame_data.frame.x.unwrap() as f32 / data.meta.size.w as f32,
-                (frame_data.frame.x.unwrap() + frame_data.frame.w) as f32 / data.meta.size.w as f32,
-                frame_data.frame.y.unwrap() as f32 / data.meta.size.h as f32,
-                (frame_data.frame.y.unwrap() + frame_data.frame.h) as f32 / data.meta.size.h as f32
-            ];
-
-            let offset = [
-                2.0 / sprite_length as f32 * vertex_index as f32,
-                0.0
-            ];
-
-            let dest_uv = [
-                offset[0] + frame_data.sprite_source_size.x.unwrap() as f32 / texture_size[0] as f32  * 2.0 - 1.0 ,
-                offset[0] + (frame_data.sprite_source_size.x.unwrap() + frame_data.sprite_source_size.w) as f32 / texture_size[0] as f32 * 2.0 - 1.0 ,
-                frame_data.sprite_source_size.y.unwrap() as f32 / texture_size[1] as f32 * 2.0 - 1.0,
-                (frame_data.sprite_source_size.y.unwrap() + frame_data.sprite_source_size.h) as f32 / texture_size[1] as f32 * 2.0 - 1.0
-            ];
-
-            vertex.push(Vertex {
-                position: [dest_uv[0], dest_uv[2], 0.0],
-                tex_coords: [original_uv[0], original_uv[3]],
-            });
-
-            vertex.push(Vertex {
-                position: [dest_uv[1], dest_uv[2], 0.0],
-                tex_coords: [original_uv[1], original_uv[3]],
-            });
-            vertex.push(Vertex {
-                position: [dest_uv[1], dest_uv[3], 0.0],
-                tex_coords: [original_uv[1], original_uv[2]],
-            });
-            vertex.push(Vertex {
-                position: [dest_uv[0], dest_uv[3], 0.0],
-                tex_coords: [original_uv[0], original_uv[2]],
-            });
-
-            indices.push(0 + vertex_index * 4);
-            indices.push(1 + vertex_index * 4);
-            indices.push(2 + vertex_index * 4);
-            indices.push(2 + vertex_index * 4);
-            indices.push(3 + vertex_index * 4);
-            indices.push(0 + vertex_index * 4);
-        }
-
-
-
-
+        //region [ Make RTT Texture And Output Buffer ]
         let texture_desc = wgpu::TextureDescriptor {
             size: wgpu::Extent3d {
                 width: texture_size[0],
@@ -196,22 +147,12 @@ impl AnimationDataHandler {
         let texture = device.create_texture(&texture_desc);
         let texture_view = texture.create_view(&Default::default());
 
-        // we need to store this for later
-        let u32_size = std::mem::size_of::<u32>() as u32;
 
-        let output_buffer_size = (u32_size * texture_size[0] * texture_size[1]) as wgpu::BufferAddress;
-        let output_buffer_desc = wgpu::BufferDescriptor {
-            size: output_buffer_size,
-            usage: wgpu::BufferUsages::COPY_DST
-                // this tells wpgu that we want to read this buffer from the cpu
-                | wgpu::BufferUsages::MAP_READ,
-            label: None,
-            mapped_at_creation: false,
-        };
-        let output_buffer = device.create_buffer(&output_buffer_desc);
+        //endregion
+
 
         let shader = device.create_shader_module(wgpu::include_wgsl!("../../assets/rtt.wgsl"));
-
+        //이거 gpu_resource_manager에서 가져와도 된다
         let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             entries: &[
                 wgpu::BindGroupLayoutEntry {
@@ -235,48 +176,156 @@ impl AnimationDataHandler {
         });
 
 
-        let diffuse_texture = Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/06_1.png"), "06_1").unwrap();
+        let mut vertex_buffers = vec![];
+        let mut index_buffers = vec![];
+        let mut indices_count = vec![];
+        let mut diffuse_bind_groups = vec![];
 
-        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
+        //region [ Load atlas data And Make Vertex, Index ]
+        let json_path = [
+            "./assets/character/04.json",
+            "./assets/character/03.json",
+            "./assets/character/02.json",
+            "./assets/character/03.json",
+            "./assets/character/04.json",
+            "./assets/character/05.json",
+            "./assets/character/06.json",
+            "./assets/character/05.json",
+        ];
 
-        let vertex_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(&vertex),
-                usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+        let textures = [
+            Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/02.png"), "").unwrap(),
+            Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/03.png"), "").unwrap(),
+            Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/04.png"), "").unwrap(),
+            Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/05.png"), "").unwrap(),
+            Texture::from_bytes(&device, &queue, include_bytes!("../../assets/character/06.png"), "").unwrap(),
+        ];
+
+
+        for i in 0..8 {
+            let str = fs::read_to_string(json_path[i]).expect("Unable to read file");
+            let data: AnimationJsonData = serde_json::from_str(&str).expect("JSON was not well-formatted");
+
+            let sprite_length = data.frames.len();
+
+            let mut vertex: Vec<Vertex> = vec![];
+            let mut indices: Vec<u16> = vec![];
+
+            for vertex_index in 0..sprite_length as u16 {
+                let frame_data = &data.frames[vertex_index as usize];
+
+
+                let original_uv = if i == 0 || i == 1 || i == 7 {
+                    [
+                        (frame_data.frame.x.unwrap() + frame_data.frame.w) as f32 / data.meta.size.w as f32,
+                        frame_data.frame.x.unwrap() as f32 / data.meta.size.w as f32,
+                        frame_data.frame.y.unwrap() as f32 / data.meta.size.h as f32,
+                        (frame_data.frame.y.unwrap() + frame_data.frame.h) as f32 / data.meta.size.h as f32,
+                    ]
+                } else {
+                    [
+                        frame_data.frame.x.unwrap() as f32 / data.meta.size.w as f32,
+                        (frame_data.frame.x.unwrap() + frame_data.frame.w) as f32 / data.meta.size.w as f32,
+                        frame_data.frame.y.unwrap() as f32 / data.meta.size.h as f32,
+                        (frame_data.frame.y.unwrap() + frame_data.frame.h) as f32 / data.meta.size.h as f32
+                    ]
+                };
+
+                let offset = [
+                    2.0 / sprite_length as f32 * vertex_index as f32,
+                    2.0 - 0.25 * (i + 1) as f32
+                ];
+
+                let dest_uv = [
+                    offset[0] + frame_data.sprite_source_size.x.unwrap() as f32 / texture_size[0] as f32 * 2.0 - 1.0,
+                    offset[0] + (frame_data.sprite_source_size.x.unwrap() + frame_data.sprite_source_size.w) as f32 / texture_size[0] as f32 * 2.0 - 1.0,
+                    offset[1] + frame_data.sprite_source_size.y.unwrap() as f32 / texture_size[1] as f32 * 2.0 - 1.0,
+                    offset[1] + (frame_data.sprite_source_size.y.unwrap() + frame_data.sprite_source_size.h) as f32 / texture_size[1] as f32 * 2.0 - 1.0
+                ];
+
+                vertex.push(Vertex {
+                    position: [dest_uv[0], dest_uv[2], 0.0],
+                    tex_coords: [original_uv[0], original_uv[3]],
+                });
+
+                vertex.push(Vertex {
+                    position: [dest_uv[1], dest_uv[2], 0.0],
+                    tex_coords: [original_uv[1], original_uv[3]],
+                });
+                vertex.push(Vertex {
+                    position: [dest_uv[1], dest_uv[3], 0.0],
+                    tex_coords: [original_uv[1], original_uv[2]],
+                });
+                vertex.push(Vertex {
+                    position: [dest_uv[0], dest_uv[3], 0.0],
+                    tex_coords: [original_uv[0], original_uv[2]],
+                });
+
+                indices.push(0 + vertex_index * 4);
+                indices.push(1 + vertex_index * 4);
+                indices.push(2 + vertex_index * 4);
+                indices.push(2 + vertex_index * 4);
+                indices.push(3 + vertex_index * 4);
+                indices.push(0 + vertex_index * 4);
             }
-        );
 
-        let index_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Index Buffer"),
-                contents: bytemuck::cast_slice(&indices),
-                usage: wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST,
-            }
-        );
+            let target_texture = if i == 0 {
+                &textures[2]
+            } else if i == 1 {
+                &textures[1]
+            } else if i == 7 {
+                &textures[3]
+            } else {
+                &textures[i - 2]
+            };
+            let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&target_texture.view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&target_texture.sampler),
+                    },
+                ],
+                label: Some("diffuse_bind_group"),
+            });
+            let vertex_buffer = device.create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer"),
+                    contents: bytemuck::cast_slice(&vertex),
+                    usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+                }
+            );
 
-        let num_indices = indices.len() as u32;
+            let index_buffer = device.create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Index Buffer"),
+                    contents: bytemuck::cast_slice(&indices),
+                    usage: wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST,
+                }
+            );
+
+            let num_indices = indices.len() as u32;
+
+            vertex_buffers.push(vertex_buffer);
+            index_buffers.push(index_buffer);
+            indices_count.push(num_indices);
+            diffuse_bind_groups.push(diffuse_bind_group);
+        }
 
 
+        //endregion
+
+
+        //prepare render
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
             bind_group_layouts: &[&texture_bind_group_layout],
             push_constant_ranges: &[],
         });
-
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -319,40 +368,57 @@ impl AnimationDataHandler {
             // indicates how many array layers the attachments will have.
             multiview: None,
         });
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        let render_pass_desc = wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &texture_view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.0,
+                        g: 0.0,
+                        b: 0.0,
+                        a: 0.0,
+                    }),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        };
 
-        let mut encoder =
-            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
 
-
+        //render in render pass
         {
-            let render_pass_desc = wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.0,
-                            g: 0.0,
-                            b: 0.0,
-                            a: 0.0,
-                        }),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            };
             let mut render_pass = encoder.begin_render_pass(&render_pass_desc);
 
             render_pass.set_pipeline(&render_pipeline);
 
-            render_pass.set_bind_group(0, &diffuse_bind_group, &[]);
-            render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-            render_pass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.draw_indexed(0..num_indices, 0, 0..1);
 
-            // render_pass.draw(0..3, 0..1);
+            for i in 0..vertex_buffers.len() {
+                render_pass.set_bind_group(0, &diffuse_bind_groups[i], &[]);
+                render_pass.set_vertex_buffer(0, vertex_buffers[i].slice(..));
+                render_pass.set_index_buffer(index_buffers[i].slice(..), wgpu::IndexFormat::Uint16);
+                render_pass.draw_indexed(0..indices_count[i], 0, 0..1);
+            }
         }
+        //rtt render finished
+
+
+        //region Output Texture to Buffer (for output files )
+
+        let u32_size = std::mem::size_of::<u32>() as u32;
+        let output_buffer_size = (u32_size * texture_size[0] * texture_size[1]) as wgpu::BufferAddress;
+        let output_buffer_desc = wgpu::BufferDescriptor {
+            size: output_buffer_size,
+            usage: wgpu::BufferUsages::COPY_DST
+                    // this tells wpgu that we want to read this buffer from the cpu
+                    | wgpu::BufferUsages::MAP_READ,
+            label: None,
+            mapped_at_creation: false,
+        };
+        let output_buffer = device.create_buffer(&output_buffer_desc);
+
 
         encoder.copy_texture_to_buffer(
             wgpu::ImageCopyTexture {
@@ -371,8 +437,11 @@ impl AnimationDataHandler {
             },
             texture_desc.size,
         );
+        //endregion
+
 
         queue.submit(Some(encoder.finish()));
+
 
         // We need to scope the mapping variables so that we can
         // unmap the buffer
--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,11 +1,10 @@
+pub use animation_data_handler::AnimationDataHandler;
 pub use gpu_resource_manager::GPUResourceManager;
 pub use mesh::{InstanceTileRaw, Mesh};
 pub use pipeline_manager::PipelineManager;
-// pub use render_texture::rtt_test_run;
 pub use renderer::RenderState;
 pub use texture::Texture;
 pub use vertex::*;
-pub use animation_data_handler::AnimationDataHandler;
 
 mod renderer;
 mod texture;
@@ -13,6 +12,5 @@ mod pipeline_manager;
 mod gpu_resource_manager;
 mod vertex;
 mod mesh;
-// mod render_texture;
 mod animation_data_handler;
 
